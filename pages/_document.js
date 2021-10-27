5d6e35b22d58d7ebbf44d2ff0f2f76cd585e39d0
Duc Minh <taducminh2004@gmail.com>
2021-10-27 14:06:13 +0700

    cdn fontawsome

--- pages/_document.js
+++ pages/_document.js
@@ -10,14 +10,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="description" content="eCommerce" />
+
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
           />
+          <script src="https://kit.fontawesome.com/a076d05399.js"></script>
           <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
-          <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
         </Head>
         <body>
           <Main />
